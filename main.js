7bd35dc14f59eb4979d71c9a32a2c9676a0b5348
kayleoss <katie_acting@live.ca>
2017-07-30 17:34:47 -0500

    name prompt & display

--- main.js
+++ main.js
@@ -22,69 +22,75 @@ function main(){
 	$('.webdev').hide();
 	$('.phototwo').hide();
 	$('.resume').hide();
+	$('.underhead').hide();
+	$('.underhead').slideDown(2000);
 	//Homepage//
 	$('.home').on("click", function(){
-	$('.head').fadeIn(5000);
-	$('.img').hide();
-	$('.slider').hide();
-	$('.about').hide();
-	$('.sectors').hide();
-	$('.media').hide();
-	$('.neshama').hide();
-	$('.mediatwo').hide();
-	$('.pride').hide()
-	$('.sectorstwo').hide();
-	$('.mediathree').hide();
-	$('.pusateri').hide();
-	$('.mediafour').hide();
-	$('.city').hide();
-	$('.mediafive').hide();
-	$('.airport').hide();
-	$('.photography').hide();
-	$('.photo').hide();
-	$('.webdev').hide();
-	$('.phototwo').hide();
-	$('.resume').hide();
-	});
+		$('.head').fadeIn(5000);
+		$('.img').hide();
+		$('.slider').hide();
+		$('.about').hide();
+		$('.sectors').hide();
+		$('.media').hide();
+		$('.neshama').hide();
+		$('.mediatwo').hide();
+		$('.pride').hide()
+		$('.sectorstwo').hide();
+		$('.mediathree').hide();
+		$('.pusateri').hide();
+		$('.mediafour').hide();
+		$('.city').hide();
+		$('.mediafive').hide();
+		$('.airport').hide();
+		$('.photography').hide();
+		$('.photo').hide();
+		$('.webdev').hide();
+		$('.phototwo').hide();
+		$('.resume').hide();
+		$('.underhead').hide();
+		$('.underhead').slideDown(2000);
+	}); 
 	//Who Am I page//
 	$('.whoami').on("click", function(){
-	$('.head').hide();
-	$('.slider').fadeIn(1200);
-	$('.about').fadeIn(2000);
-	$('.sectors').hide();
-	$('.media').hide();
-	$('.neshama').hide();
-	$('.mediatwo').hide();
-	$('.pride').hide()
-	$('.sectorstwo').hide();
-	$('.mediathree').hide();
-	$('.pusateri').hide();
-	$('.mediafour').hide();
-	$('.city').hide();
-	$('.mediafive').hide();
-	$('.airport').hide();
-	$('.photography').hide();
-	$('.photo').hide();
-	$('.webdev').hide();
-	$('.phototwo').hide();
-	$('.resume').hide();
+		$('.head').hide();
+		$('.slider').fadeIn(1200);
+		$('.about').fadeIn(2000);
+		$('.sectors').hide();
+		$('.media').hide();
+		$('.neshama').hide();
+		$('.mediatwo').hide();
+		$('.pride').hide()
+		$('.sectorstwo').hide();
+		$('.mediathree').hide();
+		$('.pusateri').hide();
+		$('.mediafour').hide();
+		$('.city').hide();
+		$('.mediafive').hide();
+		$('.airport').hide();
+		$('.photography').hide();
+		$('.photo').hide();
+		$('.webdev').hide();
+		$('.phototwo').hide();
+		$('.resume').hide();
+		$('.underhead').hide();
 	});
 	//Experiences Page//
 	$('.exp').on("click",function(){
-	$('.head').hide();
-	$('.slider').hide();
-	$('.about').hide();
-	$('.photography').hide();
-	$('.photo').hide();
-	$('.webdev').hide();
-	$('.phototwo').hide();
-	$('.sectors').fadeIn(1000);
-	$('.media').fadeIn(2000);
-	$('.sectorstwo').fadeIn(1000);
-	$('.mediathree').fadeIn(2000);
-	$('.mediafour').fadeIn(2000);
-	$('.mediafive').fadeIn(2000);
-	$('.resume').slideDown(3500);
+		$('.head').hide();
+		$('.slider').hide();
+		$('.about').hide();
+		$('.photography').hide();
+		$('.photo').hide();
+		$('.webdev').hide();
+		$('.phototwo').hide();
+		$('.sectors').fadeIn(1000);
+		$('.media').fadeIn(2000);
+		$('.sectorstwo').fadeIn(1000);
+		$('.mediathree').fadeIn(2000);
+		$('.mediafour').fadeIn(2000);
+		$('.mediafive').fadeIn(2000);
+		$('.resume').slideDown(3500);
+		$('.underhead').hide();
 
 
 	$('.media').on('click', function(){
@@ -126,26 +132,28 @@ function main(){
 	});
 	//Skills Page//
 	$('.skills').on("click", function(){
-	$('.head').hide();
-	$('.slider').hide();
-	$('.about').hide();
-	$('.sectors').hide();
-	$('.media').hide();
-	$('.neshama').hide();
-	$('.mediatwo').hide();
-	$('.pride').hide()
-	$('.sectorstwo').hide();
-	$('.mediathree').hide();
-	$('.pusateri').hide();
-	$('.mediafour').hide();
-	$('.city').hide();
-	$('.mediafive').hide();
-	$('.airport').hide();
-	$('.photo').hide();
-	$('.phototwo').hide();
-	$('.resume').hide();
-	$('.photography').slideDown(500);
-	$('.webdev').slideDown(500);
+		$('.head').hide();
+		$('.underhead').hide();
+		$('.slider').hide();
+		$('.about').hide();
+		$('.sectors').hide();
+		$('.media').hide();
+		$('.neshama').hide();
+		$('.mediatwo').hide();
+		$('.pride').hide()
+		$('.sectorstwo').hide();
+		$('.mediathree').hide();
+		$('.pusateri').hide();
+		$('.mediafour').hide();
+		$('.city').hide();
+		$('.mediafive').hide();
+		$('.airport').hide();
+		$('.photo').hide();
+		$('.phototwo').hide();
+		$('.resume').hide();
+		$('.photography').slideDown(500);
+		$('.webdev').slideDown(500);
+
 
 	$('.photography').on('click',function(){
 		$('.photo').slideDown(1000);
@@ -164,3 +172,22 @@ function main(){
 }
 
 $(document).ready(main);
+var name = prompt ('Please Enter Your Name.');
+console.log(name);
+
+if (name === " "){
+	name = prompt('Enter Your Name Please!!!');
+	var newmessage = "Wecome, "+ name ;
+	document.getElementsByClassName('underhead')[0].innerHTML = newmessage ;
+}else if (name === "  ") {
+	name = prompt('Enter Your Name Please!!!');
+	var newmessage = "Wecome, "+ name ;
+	document.getElementsByClassName('underhead')[0].innerHTML = newmessage ;
+}else if (name === "    "){
+	name = prompt('Enter Your Name Please!!!');
+	var newmessage = "Wecome, "+ name ;
+	document.getElementsByClassName('underhead')[0].innerHTML = newmessage ;
+}else{
+	var message = "Welcome, "+ name ;
+	document.getElementsByClassName('underhead')[0].innerHTML = message;
+}
